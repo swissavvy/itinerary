e9164284ba9013be3fb214b64c49ed22ad27cb7f
liv <523260513@qq.com>
2016-01-02 22:04:34 +0800

    对接接口

--- www/templates/themes/default/js/controllers.js
+++ www/templates/themes/default/js/controllers.js
@@ -1,25 +1,38 @@
 /**
  * Created by Liv on 15/10/30.
  */
-appControllers.controller('loginCtrl', function ($scope, $location) {
+appControllers.controller('loginCtrl', function ($scope, $location, $http) {
     $scope.loginForm = {
         username: "",
         password: ""
     };
 
     $scope.login = function () {
-        $scope.navigateTo('app.siteList');
+        var params = {
+            identity: $scope.loginForm.username,
+            password: $scope.loginForm.password
+        };
+        $http.post(window.globalVariable.apiDomain + '/api/user/login', params).success(function(result){
+            if(result.status == 1){
+                $scope.navigateTo('app.siteList');
+            }
+        });
     };
 })
-.controller('signUpCtrl', function ($scope, $location) {
+.controller('signUpCtrl', function ($scope, $location, $http) {
     $scope.signUpForm = {
         username: "",
         password: "",
         confirmPassword: "",
-        email: "",
+        email: ""
     };
 
     $scope.signUp = function () {
+        var params = {};
+        $http.post(window.globalVariable.apiDomain + '/api/user/register', params).success(function(){
+
+        });
+
         $scope.navigateTo('app.siteList');
     };
 })
